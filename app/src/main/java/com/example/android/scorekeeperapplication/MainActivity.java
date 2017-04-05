54dd1e68ad0cdadb8da49d7a599fd8db127aa846
mesibaja <petra.lehoczky@gmail.com>
2017-04-05 18:20:28 +0200

    Java updates

---
+++ app/src/main/java/com/example/android/scorekeeperapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.android.scorekeeperapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int PlayerA = 0;
+
+    int PlayerB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Increase the score for Player A by 1 point but no more than 11.
+     */
+
+    public void addOneForPlayerA(View v) {
+        if (PlayerA + 1 > 10) {
+            Toast.makeText(this, "Player A won!", Toast.LENGTH_LONG).show();
+        } else {
+            PlayerA = PlayerA + 1;
+        }
+        displayForPlayerA(PlayerA);
+    }
+
+    /**
+     * Increase the score for Player A by 1 point.
+     */
+
+    public void addOneFoulForPlayerA(View v) {
+        PlayerA = PlayerA + 1;
+        displayForPlayerA(PlayerA);
+    }
+    /**
+     * Increase the score for Player B by 1 point.
+     */
+    public void addOneForPlayerB(View v) {
+        if (PlayerB + 1 > 10) {
+            Toast.makeText(this, "Player B won!", Toast.LENGTH_LONG).show();
+        } else {
+        PlayerB = PlayerB + 1;
+        displayForPlayerB (PlayerB);}
+    }
+
+    /**
+     * Increase the score for Player B by 1 point.
+     */
+
+    public void addOneFoulForPlayerB(View v) {
+        PlayerB = PlayerB + 1;
+        displayForPlayerB(PlayerB);
+    }
+    /**
+     * Resets the score for both players back to 0.
+     */
+    public void resetScore(View v){
+        PlayerA = 0;
+        PlayerB = 0;
+        displayForPlayerA(PlayerA);
+        displayForPlayerB(PlayerB);
+    }
+    /**
+     * Displays the given score for Player A.
+     */
+    public void displayForPlayerA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.player_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given Foul score for Player A.
+     */
+    public void displayOneFoulForPlayerA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.player_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given Foul score for Player B.
+     */
+    public void displayOneFoulForPlayerB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.player_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
